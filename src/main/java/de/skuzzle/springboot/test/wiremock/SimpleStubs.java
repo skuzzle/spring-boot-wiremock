a88e2df8b3f771e88be626aaa20a9b9cf9558d8b
Simon Taddiken <simon.taddiken@gmail.com>
2021-05-12 09:04:36 +0200

    Fix some javadoc

--- src/main/java/de/skuzzle/springboot/test/wiremock/SimpleStubs.java
+++ src/main/java/de/skuzzle/springboot/test/wiremock/SimpleStubs.java
@@ -6,8 +6,14 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
+/**
+ * Container annotation for repeatable {@link SimpleStub} annotation.
+ *
+ * @author Simon Taddiken
+ */
 @Retention(RUNTIME)
 @Target(ElementType.METHOD)
 public @interface SimpleStubs {
+    /** All the stubs that should be added. */
     SimpleStub[] value() default {};
 }
