758fbc845bbb9059546c0600e38ff3278c91e209
Simon Taddiken <simon.taddiken@gmail.com>
2021-05-12 15:48:29 +0200

    Refactor property parsing

--- src/main/java/de/skuzzle/springboot/test/wiremock/StubTranslator.java
+++ src/main/java/de/skuzzle/springboot/test/wiremock/StubTranslator.java
@@ -1,11 +1,19 @@
 package de.skuzzle.springboot.test.wiremock;
 
+import java.util.function.BiConsumer;
+
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.MappingBuilder;
 import com.github.tomakehurst.wiremock.client.ResponseDefinitionBuilder;
 import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.matching.StringValuePattern;
 import com.github.tomakehurst.wiremock.matching.UrlPattern;
 
+/**
+ * Translates the {@link HttpStub} instance into a WireMock stub.
+ *
+ * @author Simon Taddiken
+ */
 class StubTranslator {
 
     static void configureStubOn(WireMockServer wiremock, HttpStub annotation) {
@@ -46,21 +54,9 @@ class StubTranslator {
                 nullIfEmpty(stub.onRequest().toUrlPathPattern()));
 
         final MappingBuilder requestBuilder = WireMock.request(stub.onRequest().withMethod(), urlPattern);
-        final String[] requestHeaders = stub.onRequest().containingHeaders();
-        for (final String headerAndValue : requestHeaders) {
-            final String[] parts = headerAndValue.split("=", 2);
-            requestBuilder.withHeader(parts[0], StringValuePatterns.parseFromPrefix(parts[1]));
-        }
-        final String[] queryParams = stub.onRequest().withQueryParameters();
-        for (final String paramAndValue : queryParams) {
-            final String[] parts = paramAndValue.split("=", 2);
-            requestBuilder.withQueryParam(parts[0], StringValuePatterns.parseFromPrefix(parts[1]));
-        }
-        final String[] cookies = stub.onRequest().containingCookies();
-        for (final String cookieAndValue : cookies) {
-            final String[] parts = cookieAndValue.split("=", 2);
-            requestBuilder.withCookie(parts[0], StringValuePatterns.parseFromPrefix(parts[1]));
-        }
+        parseValueArray(stub.onRequest().containingHeaders(), requestBuilder::withHeader);
+        parseValueArray(stub.onRequest().withQueryParameters(), requestBuilder::withQueryParam);
+        parseValueArray(stub.onRequest().containingCookies(), requestBuilder::withCookie);
 
         final String basicAuthUsername = nullIfEmpty(stub.onRequest().authenticatedBy().basicAuthUsername());
         final String basicAuthPassword = nullIfEmpty(stub.onRequest().authenticatedBy().basicAuthPassword());
@@ -77,6 +73,16 @@ class StubTranslator {
             requestBuilder.withRequestBody(StringValuePatterns.parseFromPrefix(requestBody));
         }
         return requestBuilder.willReturn(responseBuilder);
+    }
+
+    private static void parseValueArray(String[] array, BiConsumer<String, StringValuePattern> builder) {
+        for (final String keyAndValue : array) {
+            final String[] parts = keyAndValue.split("=", 2);
+            final String key = parts[0];
+            final String valueWithPrefix = parts[1];
+            final StringValuePattern valuePattern = StringValuePatterns.parseFromPrefix(valueWithPrefix);
+            builder.accept(key, valuePattern);
+        }
     }
 
     private static String nullIfEmpty(String s) {
